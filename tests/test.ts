1712268852389efe48b62b84e5d114be60cd15ea
Naga Raghavarapu <naga.raghavarapu@oracle.com>
2020-05-22 13:54:04 -0700

    using puppetter instead of selenium

--- tests/test.ts
+++ tests/test.ts
@@ -1,63 +1,87 @@
-import { WebDriver, Builder, Capabilities, By} from 'selenium-webdriver';
-import { should } from 'chai';
-should();
-require('chromedriver');
-const NodeHttpAdapter = require('@pollyjs/adapter-node-http');
+// import { WebDriver, Builder, Capabilities, By} from 'selenium-webdriver';
+import * as path from 'path';
+import { Polly } from '@pollyjs/core';
+import { setupMocha as setupPolly } from '@pollyjs/core';
+import PuppeteerAdapter from '@pollyjs/adapter-puppeteer';
+import FSPersister from '@pollyjs/persister-fs';
+import puppeteer from 'puppeteer';
 
-const { Polly, setupMocha: setupPolly } = require('@pollyjs/core');
-const FSPersister = require('@pollyjs/persister-fs');
-const path = require('path');
-Polly.register(NodeHttpAdapter);
+declare const expect: Chai.ExpectStatic;
+
+Polly.register(PuppeteerAdapter);
 Polly.register(FSPersister);
-describe('Selenium Demo Test Suite', () => {
-  setupPolly({
-    /* default configuration options */
-    recordIfMissing: true,
-    mode: 'replay',
-    persister: 'fs',
-    adapters: ['node-http'],
-    persisterOptions: {
-      fs: {
-        recordingsDir: path.resolve(__dirname, '../recordings')
-      }
-    },
-    matchRequestsBy: {
-      headers(headers:Record<string, string>) {
-        // `host` is unstable since the port changes between every run
-        // probably configurable with Selenium to make it stable?
-        delete headers.host;
-        
-        return headers;
+
+describe('Selenium Demo Test Suite', async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  // const context = setupPolly({
+  //   adapters: ['puppeteer'],
+  //   mode: 'replay',
+  //   // NOTE: `page` is set by jest.config.js preset "jest-puppeteer"
+  //   adapterOptions: { puppeteer: { page } },
+  //   persister: 'fs',
+  //   persisterOptions: {
+  //     fs: {
+  //       recordingsDir: path.resolve(__dirname, '../__recordings__')
+  //     }
+  //   },
+  //   matchRequestsBy: {
+  //     headers: {
+  //       exclude: ['user-agent']
+  //     }
+  //   }
+  // });
+  beforeEach(async () => {
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
+    setupPolly({
+      adapters: ['puppeteer'],
+      mode: 'record',
+      adapterOptions: { puppeteer: { page } },
+      persister: 'fs',
+      persisterOptions: {
+        fs: {
+          recordingsDir: path.resolve(__dirname, '../__recordings__')
+        }
       },
-      url: {
-        // same reason as above
-        port: false,
+      matchRequestsBy: {
+        headers: {
+          exclude: ['user-agent']
+        }
       }
-    }
-  });
-    let driver:WebDriver;
-    // time out for test execution
-    const TIMEOUT = 1200000;
-    beforeEach(async () => {
-      // initializing chrome driver
-      driver = await new Builder()
-          .withCapabilities(Capabilities.chrome())
-          .build();
-      await driver.manage().setTimeouts( { implicit: TIMEOUT, pageLoad: 
-        TIMEOUT, script: TIMEOUT } );
-      await driver.manage().window().maximize();
     });
+    // await page.setRequestInterception(true);
+
+    // const { server } = context.polly;
+
+    // server.host('http://localhost:3000', () => {
+    //   server.get('/sockjs-node/*').intercept((_:any, res:any) => res.sendStatus(200));
+    // });
+
+    await page.goto('http://localhost:3000', { waitUntil: 'networkidle0' });
+  });
 
     afterEach(async () => {
-      await driver.quit();
+      await browser.close();
     });
 
-    it('should search for hello at localhost', async () => {
-        let Url:string = 'http://localhost:3000';
-        await driver.get(Url);
-        let p = await driver.findElement(By.id('naga'));
-        console.log(p);
-        let value = await  p.getText();
-        value.should.equal('Hello From Express');
+    it('should be able to navigate to all routes', async () => {
+      const header = await page.$eval('h2', (element:any) => {
+        return element.innerText;
+      });
+  
+      // await expect(page).toMatchElement('tbody > tr', { timeout: 5000 });
+      expect(header).to.equal('Posts');
+  
+      // await expect(page).toClick('a', { text: 'Todos' });
+      // await expect(page).toMatchElement('tbody > tr', { timeout: 5000 });
+      // await expect(header).toMatch('Todos');
+  
+      // await expect(page).toClick('a', { text: 'Users' });
+      // await expect(page).toMatchElement('tbody > tr', { timeout: 5000 });
+      // await expect(header).toMatch('Users');
+  
+      // Wait for all requests to resolve, this can also be replaced with
+      // await context.polly.flush();
     });
 });
