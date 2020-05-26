9b55711275b8013bdbf0aada562225366e2422c7
Naga Raghavarapu <naga.raghavarapu@oracle.com>
2020-05-25 17:32:13 -0700

    added more assertions, lint added

--- tests/test.ts
+++ tests/test.ts
@@ -1,64 +1,57 @@
-import * as path from 'path';
-import { Polly } from '@pollyjs/core';
-import { setupMocha as setupPolly } from '@pollyjs/core';
 import PuppeteerAdapter from '@pollyjs/adapter-puppeteer';
+import { setupMocha as setupPolly, Polly } from '@pollyjs/core';
 import FSPersister from '@pollyjs/persister-fs';
+import * as path from 'path';
 import puppeteer from 'puppeteer';
 
 import { expect } from 'chai';
 
 Polly.register(PuppeteerAdapter);
 Polly.register(FSPersister);
-let browser:any;
-let page:any;
+let browser: any;
+let page: any;
 describe('Puppeteer Suite', async function() {
   setupPolly();
-  beforeEach(async function(){
-    browser = await puppeteer.launch();
+  beforeEach(async function() {
+    browser = await puppeteer.launch({headless: false, slowMo: 1000});
     page = await browser.newPage();
     await page.setRequestInterception(true);
 
     this.polly.configure({
-      recordIfMissing: true,
-      adapters: ['puppeteer'],
-      mode: 'record',
       adapterOptions: { puppeteer: { page } },
+      adapters: ['puppeteer'],
+      matchRequestsBy: {
+        headers: {
+          exclude: ['user-agent'],
+        },
+      },
+      mode: 'record',
       persister: 'fs',
       persisterOptions: {
         fs: {
-          recordingsDir: path.resolve(__dirname, '../recordings')
-        }
+          recordingsDir: path.resolve(__dirname, '../recordings'),
+        },
       },
-      matchRequestsBy: {
-        headers: {
-          exclude: ['user-agent']
-        }
-      }
+      recordIfMissing: true,
     });
     await page.goto('http://localhost:3000', {waitUntil: 'load', timeout: 0});
   });
 
-    afterEach(async function() {
+  afterEach(async function() {
       await browser.close();
     });
 
-    it('should be able to navigate to all routes', async function() {
-      // await page.setRequestInterception(true);
-      const header = await page.$eval('h2', (element:any) => {
-        return element.innerText;
-      });
-  
-      // await expect(page).toMatchElement('tbody > tr', { timeout: 5000 });
-      expect(header).to.equal('Employees');
-  
-      // await expect(page).toClick('a', { text: 'Todos' });
-      // await expect(page).toMatchElement('tbody > tr', { timeout: 5000 });
-      // await expect(header).toMatch('Todos');
-  
-      // await expect(page).toClick('a', { text: 'Users' });
-      // await expect(page).toMatchElement('tbody > tr', { timeout: 5000 });
-      // await expect(header).toMatch('Users');
-  
+  it('should be able to navigate to all routes', async function() {
+      expect(await page.$eval('h2', e => { return e.innerText; })).to.equal('Posts');
+      expect(await page.$eval('tbody > tr', e => { return e.innerText; })).to.not.equal(null);
+      const elements: any = await page.$$('a.MenuItemLink-root-243');
+      await elements[1].click();
+      expect(await page.$eval('h2', e => { return e.innerText; })).to.equal('Todos');
+      expect(await page.$eval('tbody > tr', e => { return e.innerText; })).to.not.equal(null);
+      await elements[2].click();
+      expect(await page.$eval('h2', e => { return e.innerText; })).to.equal('Users');
+      expect(await page.$eval('tbody > tr', e => { return e.innerText; })).to.not.equal(null);
+
       // Wait for all requests to resolve, this can also be replaced with
       await this.polly.flush();
     });
